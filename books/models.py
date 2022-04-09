ada18bc7bb5fb89d874482cfb9716fada3ca58ec
drsehid <rasoulisaeid92@gmail.com>
2022-04-10 00:45:02 +0430

    books

--- books/models.py
+++ books/models.py
@@ -4,7 +4,7 @@ from auth.models import User
 class Book(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    user = models.ForeignKey(User, related_name="books", on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
